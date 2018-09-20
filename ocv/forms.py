888302d6e96d9e49e9e1844f91ddd51a0aabbba2
vpanchuk <vasil.panchuk@gmail.com>
2018-09-20 23:51:38 +0300

    Second OCV interface change

--- ocv/forms.py
+++ ocv/forms.py
@@ -1,9 +1,14 @@
 from django import forms
+from django.forms import Select, TextInput
 from .models import CalculationOCV
+
 
 class CalculationOCVForm(forms.ModelForm):
     class Meta:
         model = CalculationOCV
-        fields = '__all__'
-        labels = {'setlCalc': 'Місце реєстрації авто', 'catTypeCalc': 'Тип транспортного засобу', 'price': 'Вартість '
-                                                                                                           'полісу'}
+        fields = ('setlCalc', 'catTypeCalc', 'price')
+        widgets = {
+            'setlCalc': Select(attrs={"class": "form-control", "type": "text", "placeholder": "Default input"}),
+            'catTypeCalc': Select(attrs={"class": "form-control", "type": "text", "placeholder": "Default input"}),
+            'price': TextInput(attrs={"class": "form-control", "type": "text"})
+        }
