e8a3e682614f79fc11eb94836193ae51db1f7e5d
vpanchuk <vasil.panchuk@gmail.com>
2018-09-28 23:01:21 +0300

    Change lists on main page

--- ocv/views.py
+++ ocv/views.py
@@ -1,7 +1,6 @@
 # -*- coding: utf8 -*-
 from django.shortcuts import render
 from django.views import View
-from .forms import CalculationOCVForm
 from .models import CarTypeSimple, CarTypeLabel, CarTypeMTSBU, Settlement
 
 
@@ -18,10 +17,17 @@ class OCV_View(View):
         return render(request, 'ocv/ocv_calc.html', context)
 
 
-def index(request, cartype_id):
+def index(request, cartype_id , setl_id):
     setl = Settlement.objects.all()
     cartypes = CarTypeSimple.objects.all()
+    typeDefault = CarTypeSimple.objects.get(pk=cartype_id)
     cartypelabel = CarTypeLabel.objects.get(carTypeSimple__id=cartype_id)
     cartypemtsbu = CarTypeMTSBU.objects.filter(carTypeSimple__id=cartype_id)
-    context = {'setl': setl, 'cartypes': cartypes, 'cartypelabel': cartypelabel, 'cartypemtsbu': cartypemtsbu}
+    if setl_id != '0':
+        setlDefault = Settlement.objects.get(pk=setl_id)
+        context = {'setl': setl, 'cartypes': cartypes, 'cartypelabel': cartypelabel, 'cartypemtsbu': cartypemtsbu,
+                   'typeDefault': typeDefault, 'setlDefault': setlDefault}
+    else:
+        context = {'setl': setl, 'cartypes': cartypes, 'cartypelabel': cartypelabel, 'cartypemtsbu': cartypemtsbu,
+               'typeDefault': typeDefault}
     return render(request, 'ocv/ocv_calc.html', context)
